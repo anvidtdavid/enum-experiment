47111fe4ecc1ff8b6dc1ecc6b0dcd7ea65d59e75
Anvid T David <anvid.david@fingent.com>
2019-11-27 16:24:34 +0530

    ClientApp rearranged

--- Controllers/CommonController.cs
+++ Controllers/CommonController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using EnumExperiment.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnumExperiment.Controllers
